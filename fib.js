c3e80098e286cb394718545c0815a907e6a32d2e
Kareena Hudson <kareena.k.hudson@aggiemail.usu.edu>
2017-03-17 02:52:37 +0000

    javascript document for fibonnaci

---
+++ fib.js
@@ -0,0 +1,17 @@
+function fibbonaci(n)
+{
+    var number;
+    var div = document.createElement('div');
+    
+    if (n < 2)
+    {
+        if(n===0)
+        {
+            number = 0;
+        }
+        else if(n===1)
+        {
+            number = 1;
+        }
+    }
+}
