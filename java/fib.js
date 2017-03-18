3c4ff1eaa2b84646e7b24cd2e18517191d0f8630
Kareena Hudson <kareena.k.hudson@aggiemail.usu.edu>
2017-03-18 01:51:00 +0000

    fibonnaci and pell sequences

---
+++ java/fib.js
@@ -0,0 +1,207 @@
+function fibHelper(n) {
+	var value;
+	var div = document.createElement('div');
+	div.setAttribute("class", "fib");
+
+	// leaf nodes aka. base case
+	if (n < 2) {
+		if (n === 0) {
+			value = 0;
+		} 
+		else if (n === 1) {
+			value = 1;
+		}
+		var p = document.createElement('p');
+		p.textContent = 'Fib(' + n + ') = ' + value;
+		div.appendChild(p)
+	} 
+	else {
+		var left = fibHelper(n - 1);
+		var clas = left.html.getAttribute("class");
+		left.html.setAttribute("class", clas + " fib-left");
+		
+		var right = fibHelper(n - 2);
+		clas = right.html.getAttribute("class");
+		right.html.setAttribute("class", clas + " fib-right");
+
+		value = left.value + right.value;
+		var p = document.createElement('p');
+		p.textContent = 'Fib(' + n + ') = ' + value;
+		div.appendChild(p);
+
+		div.appendChild(left.html);
+		div.appendChild(right.html);
+	}
+	return { 'value': value, 'html': div };
+}
+var fib = function (n, node) {
+	var tree = fibHelper(n)
+		node.appendChild(tree.html);
+		//node.style = "display: inline-block;";
+	  node.setAttribute("id", "fib");
+}
+
+var style = document.createElement('style');
+style.textContent = 
+	"#fib {" +
+	"	display: inline-block;" +
+	"	width: 20000px;" +
+	"}" +
+	"" +
+	".fib {" +
+	"	background-color: rgba(0,0,255,0.1);" +
+	"}" +
+	"" +
+	".fib-left {" +
+	"	float: left;" +
+	"	display: inline-block;" +
+	"	margin-right: 4px;" +
+	"}" +
+	"" +
+	".fib-right {" +
+	"	float: right;" +
+	"	display: inline-block;" +
+	"	margin-left: 4px;" +
+	"}" +
+	"" +
+	".shadowed {" +
+	"	text-shadow: 1px 1px 2px black;" +
+	"	color:       white;" +
+	"}" +
+	".stuff-box {" +
+	"	font-family: 'helvetica neue', helvetica, sans-serif;" +
+	"	letter-spacing: 1px;" +
+	"	text-transform: capitalize;" +
+	"	text-align: center;" +
+	"	padding: 3px 10px;" +
+	"	margin: 10px;" +
+	"	cursor: pointer;" +
+	"	border-radius: 10px;" +
+	"	border-width: 2px;" +
+	"	border-style: solid;" +
+	"}" +
+	"" +
+	".red {" +
+	"	border-color: rgb(255,0,0);" +
+	"	background:   rgb(180,60,60);" +
+	"	box-shadow: 1px 1px 2px rgba(200,0,0,0.4);" +
+	"}" +
+	"" +
+	".yellow {" +
+	"	border-color: rgb(255,255,0);" +
+	"	background:   rgb(180,180,60);" +
+	"	box-shadow: 1px 1px 2px rgba(200,200,0,0.4);" +
+	"}" +
+	"" +
+	".blue {" +
+	"	border-color: rgb(0,0,255);" +
+	"	background:   rgb(60,60,180);" +
+	"	box-shadow: 1px 1px 2px rgba(0,0,200,0.4);" +
+	"}" +
+	"" +
+	".green {" +
+	"	border-color: rgb(0,255,0);" +
+	"	background:   rgb(60,180,60);" +
+	"	box-shadow: 1px 1px 2px rgba(0,200,0,0.4);" +
+	"}";
+
+document.querySelector('body').appendChild(style);
+
+// IIFE = Immediately-invoked function expression
+// An anonymous function which we create, invoke, then throw away.
+// It exists to meet these aims:
+//   0) Avoid clobbering an existing variable named "div" in the global namespace. I don't have
+//      another variable called "div" today, but this protects my webpage in the event that I
+//      later incorporate other JavaScript programs from elsewhere.
+//   1) Collect a few related program statements and keep them together in a single unit.
+// https://en.wikipedia.org/wiki/Immediately-invoked_function_expression
+( function(color, id) {
+	var div = document.createElement('div');
+	div.setAttribute('class', color + ' shadowed stuff-box');
+	div.setAttribute('id', id);
+	document.body.appendChild(div);
+}('red', 'fib'));
+
+fib(9, document.querySelector('.red'))
+
+
+// divMakerMaker() is a function which returns a function
+// divMakerMaker() takes two arguments and creates a function which requires
+// no arguments of its own, but upon invocation "remembers" the functions it
+// was created with
+var divMakerMaker = function(color, id) {
+	return function() {
+		var div = document.createElement('div');
+		div.setAttribute('class', color + ' shadowed stuff-box');
+		div.setAttribute('id', id);
+		document.body.appendChild(div);
+	}
+}
+
+var blueDiv = divMakerMaker('blue', 'fib');
+var yellowDiv = divMakerMaker('yellow', 'yomama');
+
+blueDiv();
+yellowDiv();
+
+fib(10, document.querySelector('.blue'))
+fib(11, document.querySelector('.yellow'))
+
+function pellhelper(n)
+{
+    var value;
+	var div2 = document.createElement('div');
+	div2.setAttribute("class", "pell");
+
+   if (n < 2)
+   {
+       if(n===0)
+       {
+           value = 0;
+       }
+       else if(n===1)
+       {
+           value = 1;
+       }
+       	var p = document.createElement('p');
+		p.textContent = 'pell(' + n + ') = ' + value;
+		div2.appendChild(p);
+   }
+   else{
+       	var left = pellhelper(n - 1);
+		var clas = left.html.getAttribute("class");
+		left.html.setAttribute("class", clas + "pell-left");
+		
+		var right = pellhelper(n - 2);
+		clas = right.html.getAttribute("class");
+		right.html.setAttribute("class", clas + " pell-right");
+
+		value = 2*(left.value) + right.value;
+		var p = document.createElement('p');
+		p.textContent = 'Pell(' + n + ') = ' + value;
+		div2.appendChild(p);
+
+		div2.appendChild(left.html);
+		div2.appendChild(right.html);
+   }
+   return { 'value': value, 'html': div2 };
+}
+var pell = function (n, node) {
+	var tree = pellhelper(n)
+		node.appendChild(tree.html);
+		//node.style = "display: inline-block;";
+	  node.setAttribute("id", "pell");
+}
+
+
+document.querySelector('body').appendChild(style);
+
+pell(9, document.querySelector('.red'))
+
+var blueDiv = divMakerMaker('blue', 'pell');
+var yellowDiv = divMakerMaker('yellow', 'pell');
+
+
+pell(10, document.querySelector('.blue'))
+pell(11, document.querySelector('.yellow'))
+
